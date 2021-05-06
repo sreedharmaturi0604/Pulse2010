ac0cdfb855c86968236784f03f10cf472f0e3a2a
Sreedhar <sreedharmaturi3@gmail.com>
2021-05-06 16:55:47 +0530

    Created a new pulse iOS project.

---
+++ PulseProjectForGit/PulseProjectForGit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PulseProjectForGit
+//
+//  Created by ADMINI on 5/6/21.
+//  Copyright © 2021 ADMINI. All rights reserved.
+
+//Doing some changes
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
